091c282dbb0d1c3754b3007fe025e8e07bff3500
hfarazul <h.farazul@gmail.com>
2022-05-22 02:25:45 +0530

    Gas optimization Assignment

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,14 +1,15 @@
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Example3 {
-    uint256 marginPercentage = 30;
     function getOwnerMargin(uint256 amount)
         public
         view
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+        amountForSender = (amount * 70) / 100;
+        amountForOwner = (amount * 30) / 100;
     }
 
 }
