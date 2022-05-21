44a37c6c618103a3d8280b3d70728f9a79fbd5a8
Gates Porter <gatesporter8@Gatess-Air.attlocal.net>
2022-05-21 14:51:06 -0500

    optimizations

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -2,10 +2,15 @@ pragma solidity ^0.8.4;
 
 contract Example2 {
     uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+
+    function incrementBy(uint[] calldata arr) external {
+        uint _counter = counter;
+        for (uint idx = 0; idx < arr.length; ) {
+            _counter += arr[idx];
+            unchecked {
+                ++idx;
+            }
         }
+        counter = _counter;
     }
 }
