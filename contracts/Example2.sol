328573f4936d13fdf617424d5e2761879e81325b
Srigowri <srigowri.venkatesha@gmail.com>
2022-05-22 10:57:04 +0530

    optimized gas

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -1,11 +1,18 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Example2 {
     uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    function incrementBy(uint[] calldata arr) public {
+        // uint idx;
+        uint count;        
+        uint length = arr.length;
+        for (uint idx=0; idx < length; ) {
+            count += arr[idx];
+            unchecked {
+                idx++;
+            }
         }
+        counter = count;
     }
 }
--- test/Example2.test.js
+++ test/Example2.test.js
@@ -26,6 +26,7 @@ describe("Example2", function () {
 
   it("should pass the gas test", async function() {
     const currentGas = (await example.estimateGas.incrementBy(arr)).toNumber();
+    console.log(currentGas);
     assert(currentGas < BASE_GAS);
   });
 });
