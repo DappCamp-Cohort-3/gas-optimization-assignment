9b019734009a19b48734bf39e4571329f262e78a
tony <sodexx7@gmail.com>
2022-05-23 15:34:15 +0800

    Test passed

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.8.4;
 
 contract Example3 {
-    uint256 marginPercentage = 30;
+    uint32 constant MARGINPERCENTAGE = 30;
+
     function getOwnerMargin(uint256 amount)
         public
-        view
+        pure
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+        uint256 _amountForSender = (amount * (100 - MARGINPERCENTAGE)) / 100;
+        return (_amountForSender, amount - _amountForSender);
     }
-
 }
