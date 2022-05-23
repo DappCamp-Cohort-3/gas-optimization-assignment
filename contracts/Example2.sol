736fcbee9a084fcd6f46d5fa215c7e162a5459e0
Parshvi <parshvisri@gmail.com>
2022-05-23 10:01:16 +0530

    Assignment Solution

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    function incrementBy(uint[] calldata arr) external {
+        uint _counter=counter;
+        uint arrLength= arr.length;
+        for (uint idx ; idx < arrLength;){
+            _counter += arr[idx];
+            unchecked{
+            ++idx;
         }
+        }
+        
+        counter = _counter;
     }
 }
