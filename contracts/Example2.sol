a5c31c45b1d51f3d9774dc4beda0cb6a9112ca0b
Thomas Hepner <hepner.thomas@gmail.com>
2022-05-22 21:36:20 -0700

    fix: Example2 function changed from public to external.

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] calldata arr) public {
+    function incrementBy(uint[] calldata arr) external {
         uint length = arr.length;
         uint _counter;
         for (uint idx = 0; idx < length;){
-            _counter = _counter + arr[idx];
+            _counter += arr[idx];
             unchecked {
                 ++idx;
             }            
