1845928983582361048890c46febbe8e0b92691a
Mayuresh Bakshi <mayureshpr@gmail.com>
2022-05-24 01:06:32 +0530

    gas optimized and tested on remix

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -1,14 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 contract Example3 {
-    uint256 marginPercentage = 30;
+    //uint256 marginPercentage = 30;
     function getOwnerMargin(uint256 amount)
         public
-        view
+        pure
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+        amountForSender = (amount * (100 - 30)) / 100;
+        amountForOwner = (amount * 30) / 100;
+        return (amountForSender, amountForOwner);
     }
 
 }
+
+//219659
+//183277
