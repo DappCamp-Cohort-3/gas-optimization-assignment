c67572ec4d0a5c4e43569a7cc316ed1c4c470c1d
Jason Chaskin <jasonchaskin@Jasons-MacBook-Air.local>
2022-05-26 11:45:48 -0500

    Jason_Chaskin gas-optimization-assignment

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -2,10 +2,13 @@ pragma solidity ^0.8.4;
 
 contract Example2 {
     uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+
+    function incrementBy(uint[] calldata arr) external {
+        uint length = arr.length;
+        uint updatedCounter;
+        for (uint idx = 0; idx < length; ++idx) {
+            updatedCounter += arr[idx];
         }
+        counter = updatedCounter;
     }
 }
--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.8.4;
 
 contract Example3 {
     uint256 marginPercentage = 30;
+
     function getOwnerMargin(uint256 amount)
-        public
+        external
         view
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+        uint256 marginPercentageLocal = 30;
+        amountForSender = (amount * (100 - marginPercentageLocal)) / 100;
+        amountForOwner = (amount * marginPercentageLocal) / 100;
     }
-
 }
