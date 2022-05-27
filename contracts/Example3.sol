fd872405ce2a37e5b7a835e95b5d6d5331ceec0f
SaumoPal97 <saumopal1997@gmail.com>
2022-05-28 02:18:48 +0530

    fix: add gas optimizations

--- contracts/Example3.sol
+++ contracts/Example3.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.4;
 contract Example3 {
     uint256 marginPercentage = 30;
     function getOwnerMargin(uint256 amount)
-        public
+        external
         view
         returns (uint256 amountForSender, uint256 amountForOwner)
     {
-        amountForSender = (amount * (100 - marginPercentage)) / 100;
-        amountForOwner = (amount * marginPercentage) / 100;
+        uint _marginPercentage = marginPercentage;
+        amountForSender = (amount * (100 - _marginPercentage)) / 100;
+        amountForOwner = amount - amountForSender;
     }
 
 }
