a65ea95b17fbf563f61e69ce7de4428f05904b70
kai237 <amansidhant@gmail.com>
2022-06-03 23:38:17 -0700

    tests completed

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    function incrementBy(uint[] calldata arr) external {
+        uint _counter;
+        uint[] calldata _arr = arr;
+        for (uint idx = 0; idx < _arr.length; idx++){
+            _counter += _arr[idx];
+            
         }
+        counter = _counter;
     }
 }
