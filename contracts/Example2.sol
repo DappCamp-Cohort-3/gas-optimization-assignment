682de8d35dd20e98b807b54d1fef1d3ae4b00eb6
blackqueentech <deliandjam@gmail.com>
2022-06-19 20:12:52 -0700

    passing tests

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,18 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+    function incrementBy(uint[] calldata arr) public {
+        uint length = arr.length;
+        uint temp = counter;
+        for (uint idx = 0; idx < length; idx = unchecked_inc(idx)){
+            temp += arr[idx];
+        }
+        counter = temp;
+    }
+
+    function unchecked_inc(uint i) public returns (uint) {
+        unchecked {
+            return i + 1;
         }
     }
 }
